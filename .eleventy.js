f58c2a33feee846b7da1a55a06d012eeaae85c47
Fahrni Daniel <asdasd.asd@asdads>
2024-06-01 13:15:38 +0200

    change content

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,9 @@ module.exports = function(eleventyConfig) {
     // Copy the `css/styles.css` file directly to the output
     eleventyConfig.addPassthroughCopy({ "css/styles.css": "styles.css" });
 
+    // Ignoriere den `#Doku` Ordner
+    eleventyConfig.ignores.add("#Doku");
+
     return {
         dir: {
             input: ".",
